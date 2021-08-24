9bb9c53d03282a85fbf2e7d8d1c63231f78e3ccc
Jason Salerno <jason@meltedspork.com>
2021-08-23 23:05:58 -0500

    Add dog image randomizer fetch api

--- src/components/DogImage.js
+++ src/components/DogImage.js
@@ -1,6 +1,15 @@
-import dogImage from '../assets/dog.jpg';
+import { useState } from 'react';
+import defaultDogImage from '../assets/dog.jpg';
+
+const DOG_IMAGE_RANDOMIZER_API = 'https://dog.ceo/api/breeds/image/random';
 
 const Dog = (_props) => {
+  const [dogImage, _setDogImage] = useState(defaultDogImage);
+
+  fetch(DOG_IMAGE_RANDOMIZER_API)
+    .then(response => response.json())
+    .then(data => console.log(data));
+
   return (
     <div>
       <img src={dogImage} alt="Logo" />
